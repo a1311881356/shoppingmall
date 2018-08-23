6527e92fd6ab90df63af524ab753b0619eedbafc
lbw <1311881356@qq.com>
2018-08-23 16:00:31 +0800

    上次商家后台管理静态资源添加错了，这修改资源

---
+++ merchant-web/src/main/java/com/goddess/merchantweb/MerchantWebApplication.java
@@ -0,0 +1,12 @@
+package com.goddess.merchantweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantWebApplication.class, args);
+	}
+}
