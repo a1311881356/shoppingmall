d1249151ec62a527c2af3982aa2ff6f3f6268621
“ <2076756008@qq.com>
2018-08-23 17:04:56 +0800

    user模块接口首次提交

--- user/src/main/java/com/goddess/user/UserApplication.java
+++ user/src/main/java/com/goddess/user/UserApplication.java
@@ -1,9 +1,16 @@
 package com.goddess.user;
 
+import com.goddess.util.IndustrySMS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+import java.util.Random;
+
+@SpringBootApplication()
+@EnableEurekaClient
+@ComponentScan(value="com.goddess")
 public class UserApplication {
 
 	public static void main(String[] args) {
